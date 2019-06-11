1d99b0a68b7417f976c1b2a63f4557abfdf897dc
qyxxxigd <qyigakki@gmail.com>
2019-06-10 22:16:38 -0400

    load_data_catagory.py

--- ETL/load_data_catagory.py
+++ ETL/load_data_catagory.py
@@ -10,9 +10,28 @@ groups_table = db.Table('groups', metadata, autoload=True, autoload_with=engine)
 controllers_table = db.Table('controllers', metadata, autoload=True, autoload_with=engine)
 
 input_dir = "/home/ubuntu/CAtS/ETL/nist80053_rev4/"
-input_file = input_dir + "NIST_SP-800-53_rev4_catalog-min.json"
+input_catalog = input_dir + "NIST_SP-800-53_rev4_catalog-min.json"
+input_high = input_dir + "NIST_SP-800-53_rev4_HIGH-baseline_profile-min.json"
+input_moderate = input_dir + "NIST_SP-800-53_rev4_MODERATE-baseline_profile-min.json"
+input_low = input_dir + "NIST_SP-800-53_rev4_LOW-baseline_profile-min.json"
 
-with open(input_file, 'r') as fin:
+
+def get_controller_priority(input_file):
+    ids_res = set()
+    with open(input_file, 'r') as f:
+        controllers_data = json.load(f)
+        ids = controllers_data['profile']['imports'][0]['include']['id-selectors']
+        for item in ids:
+            id = item.get('control-id', item.get('subcontrol-id'))
+            ids_res.add(id)
+    return ids_res
+
+
+controllers_high = get_controller_priority(input_high)
+controllers_moderate = get_controller_priority(input_moderate)
+controllers_low = get_controller_priority(input_low)
+
+with open(input_catalog, 'r') as fin:
     data = json.load(fin)
     controller_groups = data['catalog']['groups']
     for group in controller_groups:
@@ -32,6 +51,9 @@ with open(input_file, 'r') as fin:
             links = controller.get('links', None)
             parts = controller.get('parts', None)
             pid = controller.get('pid', None)
+            high = cid in controllers_high
+            moderate = cid in controllers_moderate
+            low = cid in controllers_low
             subcontrollers = controller.get('subcontrols', [])
             for sid in subcontrollers:
                 subcontrol = subcontrollers[sid]
@@ -40,5 +62,6 @@ with open(input_file, 'r') as fin:
             insert_controller_query = db.insert(controllers_table)\
                 .values(cid=cid, gid=gid, title=ctitle, pid=pid,
                         parameters=params, properties=props,
-                        links=links, parts=parts, classinfo=classinfo)
+                        links=links, parts=parts, classinfo=classinfo,
+                        high=high, moderate=moderate, low=low)
             ICResProxy = connection.execute(insert_controller_query)
