10eba53181dc2285f8d410d74cb36061f8a48c5d
qyxxxigd <qyigakki@gmail.com>
2019-06-10 21:38:15 -0400

    load_data_catagory.py

---
+++ ETL/set-up.sh
@@ -0,0 +1,7 @@
+# sudo apt-get install postgresql
+sudo apt-get install python-psycopg2
+sudo apt-get install libpq-dev
+pip install psycopg2
+pip install sqlalchemy
+sudo apt install jupyter-core
+pip install jupyter
