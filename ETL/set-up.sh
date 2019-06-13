d701e7e8b4307c1192168ef83b48db9d183270a8
qyxxxigd <qyigakki@gmail.com>
2019-06-13 16:52:51 -0400

    remove .idea

--- ETL/set-up.sh
+++
@@ -1,7 +0,0 @@
-# sudo apt-get install postgresql
-sudo apt-get install python-psycopg2
-sudo apt-get install libpq-dev
-pip install psycopg2
-pip install sqlalchemy
-sudo apt install jupyter-core
-pip install jupyter
