7de2f9ee7ff484c9598d2d6b00fc34f94c33503b
Tong Xiao <xiaotong1006@gmail.com>
2019-07-17 21:24:39 -0400

    First version of receiving report API

--- CAS_WEB/cas/views.py
+++ CAS_WEB/cas/views.py
@@ -309,12 +309,6 @@ def project_dashboard(request):
 def simple_upload(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
-        # fs = FileSystemStorage()
-        # filename = fs.save(myfile.name, myfile)
-        # uploaded_file_url = fs.url(filename)
-        # return render(request, 'core/simple_upload.html', {
-        #     'uploaded_file_url': uploaded_file_url
-        # })
 
         save_path = 'Files'
 
