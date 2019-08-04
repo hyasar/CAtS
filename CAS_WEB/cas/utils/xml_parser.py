29fd4faf345b2158bb0471792b0aa75a2818366a
tongxiao <xiaotong1006@gmail.com>
2019-08-03 20:14:18 -0400

    Parse report API

--- CAS_WEB/cas/utils/xml_parser.py
+++ CAS_WEB/cas/utils/xml_parser.py
@@ -1,10 +1,13 @@
 from ..models import *
 from xml.etree import ElementTree as ET
 
-def parseReportXML(file, project, report_version):
 
-    report = Report(project=project, report_version=report_version)
+def parseReportXML(file, project, report_version):
+    report = Report(project=project, version=report_version)
     report.save()
+
+    print("-----------===============----------")
+    print(file.name)
 
     tree = ET.parse(file)
     root = tree.getroot()
@@ -13,13 +16,17 @@ def parseReportXML(file, project, report_version):
     date = date_info[13:23]
 
     for bug in root:
+
+        if bug.tag != 'BugInstance':
+            continue
         method, location, group, code, severity, message, _ = bug
+
         location = location[0]
         sourcefile, startLine, endLine = location.find('SourceFile'), \
                                          location.find('StartLine'), location.find('EndLine')
 
         rule = message.text.strip('.').split(' ')
-        issue = XMLIssue(report=report, version=report_version, created_time=date, sourcefile=sourcefile.text, \
+        issue = XMLIssue(report=report, created_time=date, sourcefile=sourcefile.text, \
                          startLine=startLine.text, endLine=endLine.text, group=group.text, code=code.text, \
                          severity=severity.text, rule=rule)
         issue.save()
@@ -29,7 +36,7 @@ def parseReportXML(file, project, report_version):
 
 def searchIssueXML(ControlConfigure, version):
     target_report = Report.objects.filter(project=ControlConfigure.project, version=version)
-    all_issues = Issue.objects.filter(report=target_report)
+    all_issues = XMLIssue.objects.filter(report=target_report)
 
     issues = []
     for issue in all_issues:
