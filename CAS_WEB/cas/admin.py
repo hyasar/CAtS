ce9bed21916252b5a6dc6dd0db1850763be9d69b
Qi Huairui <huairuiq@andrew.cmu.edu>
2020-06-03 16:31:11 -0400

    admin dashboard design

--- CAS_WEB/cas/admin.py
+++ CAS_WEB/cas/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Project, Control
 
-@admin.register(Project, Control)
-class SuperAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(Project, Control)
+# class SuperAdmin(admin.ModelAdmin):
+#     pass
+
+class ProjectAdmin(admin.ModelAdmin):    
+    list_display = ('name', 'description', 'created_time', 'updated_time')
+    list_filter = ('created_time', )
+    search_fields = ('name', )
+    
+class ControlAdmin(admin.ModelAdmin):
+    list_display = ('title', 'id')
+    list_filter = ('id', )
+    search_fields = ('title', 'id')
+    
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Control, ControlAdmin)
