16b5739220f5719cc107f04aeea2c70a666d97e7
zyihong <zyihong@umich.edu>
2020-07-19 03:59:28 +0000

    cas web service deploy script

--- azure-deploy/deploy.sh
+++ azure-deploy/deploy.sh
@@ -1,13 +1,37 @@
 #!/bin/sh
 
-echo "<--------------Create Helm chart and config--------------->"
+echo "<---------------Variables--------------->"
+# Change the names here
+ACR="zyhacr"
+RESOURCEGROUP="MyResourceGroup"
+AKSNAME="MyAKS"
+
+echo "<---------------Create AKS--------------->"
+az group create --name $RESOURCEGROUP --location eastus
+az acr create --resource-group $RESOURCEGROUP --name $ACR --sku Basic
+sudo az acr login --name $ACR
+az aks create -g $RESOURCEGROUP -n $AKSNAME --location eastus  --attach-acr $ACR --generate-ssh-keys
+az aks install-cli
+sudo az aks get-credentials --resource-group $RESOURCEGROUP --name $AKSNAME
+
+echo "<---------------Build and Upload Image--------------->"
+ACRSERVER=$(az acr show -n $ACR --resource-group MyResourceGroup --query loginServer | tr -d '"')
+
+# You may want to change the name and tag of the image here
+cd ~/CAtS/CAS_WEB
+sudo docker build -f Dockerfile -t caswebservice:new .
+cd ../azure-deploy
+sudo docker tag caswebservice:new $ACRSERVER/caswebservice:v1
+sudo docker push $ACRSERVER/caswebservice:v1
+
+echo "<---------------Create Helm chart and config--------------->"
 helm create caswebservice
 cp CASWEB/values.yaml caswebservice/
 cp CASWEB/Chart.yaml caswebservice/
 cp CASWEB/deployment.yaml caswebservice/templates/
 cp CASWEB/service.yaml caswebservice/templates/
 
-echo "<--------------Deploy with Helm--------------->"
+echo "<---------------Deploy with Helm--------------->"
 sudo helm install caswebservice caswebservice/
 sleep 60
 sudo kubectl get pods
